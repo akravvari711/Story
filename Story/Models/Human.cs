fdd195e504da1918293af8855b129b5e54cff048
Amalia Kravvari <amalia.kravvari@Peoplecert.onmicrosoft.com>
2020-11-05 21:20:28 +0200

    Add project files.

---
+++ Story/Models/Human.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Story.Models
+{
+    class Human
+    {
+        private string _name;
+
+        public string Name
+        {
+            get { return (this._name);}
+            set { this._name = value; }
+        }
+
+        public Human()
+        {
+
+        }
+
+        public void John()
+        {
+            Console.WriteLine("John has a house.");
+        }
+
+        public void Anna()
+        {
+            Console.WriteLine("Hi John.");
+        }
+
+
+
+    }
+}
---
+++ Story/Program.cs
@@ -0,0 +1,24 @@
+﻿using Story.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Story
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Human member1 = new Human();
+            member1.John();
+            Console.WriteLine(member1);
+
+
+            Human member2 = new Human();
+            member2.Anna();
+            Console.WriteLine(member2);
+        }
+    }
+}
